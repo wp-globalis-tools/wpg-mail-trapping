3a9e8dab309c4038eeeff2a126d4586f49043ef2
Pierre Dargham <pierre.dargham@globalis-ms.com>
2017-04-25 12:18:24 +0200

    Add environment / site url in mail subject

--- wpg-mail-trapping.php
+++ wpg-mail-trapping.php
@@ -17,14 +17,19 @@ if (WP_ENV !== 'production' && defined('WPG_MAIL_TRAPPING') && false != WPG_MAIL
 	add_filter('wp_mail', function($args) {
 		$recipients = @unserialize(WPG_MAIL_TRAPPING);
 		if(is_array($args['to'])) {
-			$original_recipients = implode(',',$args['to']);
+			$original_recipients = implode(', ', $args['to']);
 		}
 		else {
 			$original_recipients = $args['to'];
 		}
 		if(is_array($recipients)) {
+			if(defined('WP_HOME')) {
+				$env = str_replace(['http://', 'https://'], ['', ''], WP_HOME);
+			} else {
+				$env = WP_ENV;
+			}
 			$args['to'] = $recipients;
-			$args['subject'] = '[Original mail sent to : '.$original_recipients.'] '.$args['subject'];
+			$args['subject'] = '[' . $env . ' : Mail to '.$original_recipients.'] '.$args['subject'];
 		}
 		return $args;
 	});
